58c1b055a12052c77bac7f1b5b3bfa5c9c569626
Rich Przekop <richprze@gmail.com>
2013-07-10 02:06:30 +0000

    grader.js now takes in url, updated tags in checks.json

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
+var rest = require('restler');
+var request = require('request');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,10 +39,11 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-// loads html file into cheerio. cheerio categorizes by tag
-var cheerioHtmlFile = function(htmlfile) {
+
+var loadCheerio = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 
 // parses checks.json file. in this case = array of html tags
 var loadChecks = function(checksfile) {
@@ -47,7 +51,9 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+    // $ = cheerio.load(htmlstring);
+    $ = loadCheerio(htmlfile);
+
     // an array of html tags to check
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -63,6 +69,28 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+var checkHtmlUrl = function(weburl, checksfile) {
+    request(weburl, function(err, resp, html) {
+	$ = cheerio.load(html);
+
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	// for each tag in checks array
+	for(var ii in checks) {
+	    // test if tag is present in html file (which is in cheerio).
+	    // if tag from checks is present in $ (cheerio) then length 
+	    // will be 1 or higher. so check if length > 0
+	    var present = $(checks[ii]).length > 0;
+	    // out array where key = tag and value = true/false
+	    out[checks[ii]] = present;
+	}
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+    });
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -73,12 +101,20 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <web_url>', 'Website URL')
 	.parse(process.argv);
-    // returns out array (tag = key, value = true/false
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    // format out array into JSON string; 4 = # spaces
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url) {
+	// var checkJson = checkHtmlFile(program.file, program.checks);
+	checkHtmlUrl(program.url, program.checks);
+    } else { // use .file, which has a default
+	// var checkJson = checkHtmlFile(program.file, program.checks);
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	// format out array into JSON string; 4 = # spaces
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
