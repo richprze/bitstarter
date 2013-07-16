b7bedb5db0982d68981bee183c63507f59b45d09
Rich Przekop <richprze@gmail.com>
2013-07-16 23:57:40 +0000

    updated index with boostrap for part 1 hw4

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080; // 5000 for heroku
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
